17858c4a605d4a3f40220495934fcfbd528af814
rcanfield <Rcanfield86@gmail.com>
2024-05-29 16:05:38 -0400

    Remove react fragment

--- apps/mfe-monorepo/src/app/App.tsx
+++ apps/mfe-monorepo/src/app/App.tsx
@@ -41,11 +41,7 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
