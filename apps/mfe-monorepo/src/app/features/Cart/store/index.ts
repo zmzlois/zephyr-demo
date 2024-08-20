d10d1d9e59aa55582841c387a12aeae305789cc1
rcanfield <Rcanfield86@gmail.com>
2024-08-20 14:19:00 -0400

    Add random test files

--- apps/mfe-monorepo/src/app/features/Cart/store/index.ts
+++ apps/mfe-monorepo/src/app/features/Cart/store/index.ts
@@ -1,8 +1,27 @@
-import { atom } from "jotai";
-import { type Cart } from "../../../types/Cart";
+import { atom } from 'jotai';
+import { type Cart } from '../../../types/Cart';
+import { test } from 'src/app/data/ProductData';
 
 const cartAtom = atom<Cart>({
-	products: [],
+  products: [],
 });
 
 export { cartAtom };
+
+export class TestClass {
+  whatever = 'whatever';
+
+  constructor() {
+    console.log('TestClass');
+  }
+
+  public testMethod() {
+    console.log('testMethod');
+  }
+}
+
+const TestModule = {
+  testMethod() {
+    console.log('testMethod');
+  }
+}
